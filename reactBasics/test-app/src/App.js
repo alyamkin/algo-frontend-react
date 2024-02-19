f92ca1ccc9bf9eb8d07f910263a24a9d4af554a4
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-02-19 09:28:39 -0500

    add reactBasics

---
+++ reactBasics/test-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello />
+    </div>
+  );
+}
+
+function SayHello() {
+  return <p>Hello World!!!</p>;
+}
+
+export default App;
