b453bb7186a9580bb160be07f6e5b9b0255770fc
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-03-25 10:01:07 -0400

    Add portals

---
+++ portals/test-app/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import './App.css';
+
+function App() {
+  const [isHidden, setIsHidden] = useState(true);
+  return (
+    <>
+      <div className="container" onClick={() => console.log('Click container')}>
+        <button onClick={() => setIsHidden(!isHidden)}>
+          {isHidden ? 'Show Modal' : 'Hide Modal'}
+        </button>
+        {isHidden || <Modal />}
+      </div>
+
+      <p className="other">Other Content</p>
+    </>
+  );
+}
+
+export default App;
+
+function Modal() {
+  return createPortal(
+    <p className="modal">Modal</p>,
+    document.getElementById('modal-root')
+  );
+}
